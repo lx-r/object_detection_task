0228a2dac924e51cdc2c124387b150ccf9e03352
lxrobot <1287193250@qq.com>
2021-05-16 22:53:29 +0800

    add unitest

--- transformer_from_scratch/test_main.py
+++ transformer_from_scratch/test_main.py
@@ -1,12 +1,14 @@
 import torch
 from transformer import Transformer
+import unittest
 
-def main():
-    src = torch.rand(64, 16, 512)
-    tgt = torch.rand(64, 16, 512)
-    out = Transformer()(src, tgt)
-    print(out.shape)
-    # torch.Size([64, 16, 512])
+class TransformerTest(unittest.TestCase):
+    def test_main(self):
+        src = torch.rand(64, 16, 512)
+        tgt = torch.rand(64, 16, 512)
+        out = Transformer()(src, tgt)
+        print(out.shape)
+        # torch.Size([64, 16, 512])
 
 if __name__=='__main__':
-    main()
+    unittest.main()
