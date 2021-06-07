ca2d86e7caaa2132cb54a5058a810a2f6d072049
lxrobot <1287193250@qq.com>
2021-06-07 23:01:54 +0800

    code of gan adjustment

--- cv_examples/tf_tutorials/gan_tutorials/gan.py
+++ cv_examples/tf_tutorials/gan_tutorials/gan.py
@@ -111,7 +111,57 @@ def get_anim_file():
     import IPython
     if IPython.version_info > (6, 2, 0, ''):
         display.Image(filename=anim_file)
-        
+
+
+def train_step(images, generator, generator_optimizer, discriminator, discriminator_optimizer, noise_dim):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+        gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+        generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+        discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+
+
+def train(dataset, epochs, generator, generator_optimizer, discriminator, discriminator_optimizer):
+    noise_dim = 100
+    checkpoint_dir = "./training_checkpoints"
+    checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
+    checkpoint = tf.train.Checkpoint(generator_optimizer = generator_optimizer,
+                                discriminator_optimizer = discriminator_optimizer,
+                                generator = generator,
+                                discriminator = discriminator)
+    num_examples_to_generate = 16
+    seed = tf.random.normal([num_examples_to_generate, noise_dim])
+    for epoch in range(epochs):
+        start = time.time()
+        num = 0
+        for image_batch in dataset:
+            num += len(image_batch)
+            print(f"[ {epoch+1} ]===>>> total image path numbers: {num}")
+            train_step(image_batch, generator, generator_optimizer, discriminator, discriminator_optimizer, noise_dim)
+        display.clear_output(wait=True)
+        generate_and_save_images(
+            generator,
+            epoch+1,
+            seed
+        )
+        if (epoch+1) % 15 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+    display.clear_output(wait=True)
+    generate_and_save_images(
+        generator,
+        epochs,
+        seed
+    )
 
 if __name__=="__main__":
     generator = make_generator_model()
@@ -121,60 +171,8 @@ if __name__=="__main__":
     discriminator = make_discriminator_model()
     decision = discriminator(generated_image)
     print(decision)
-
     plt.imshow(generated_image[0,:,:,0], cmap='gray')
     # plt.show()
-    checkpoint_dir = "./training_checkpoints"
-    checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
-    checkpoint = tf.train.Checkpoint(generator_optimizer = generator_optimizer,
-                                    discriminator_optimizer = discriminator_optimizer,
-                                    generator = generator,
-                                    discriminator = discriminator)
-    
-    EPOCHS = 10
-    noise_dim = 100
-    num_examples_to_generate = 16
-    seed = tf.random.normal([num_examples_to_generate, noise_dim])
-    @tf.function
-    def train_step(images):
-        noise = tf.random.normal([BATCH_SIZE, noise_dim])
-        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-            generated_images = generator(noise, training=True)
-
-            real_output = discriminator(images, training=True)
-            fake_output = discriminator(generated_images, training=True)
-
-            gen_loss = generator_loss(fake_output)
-            disc_loss = discriminator_loss(real_output, fake_output)
-
-            gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-            gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-            generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-            discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-
-
-    def train(dataset, epochs):
-        for epoch in range(epochs):
-
-            start = time.time()
-            num = 0
-            for image_batch in dataset:
-                num += len(image_batch)
-                print(f"===>>> total image path numbers: {num}")
-                train_step(image_batch)
-            display.clear_output(wait=True)
-            generate_and_save_images(
-                generator,
-                epoch+1,
-                seed
-            )
-            if (epoch+1) % 15 == 0:
-                checkpoint.save(file_prefix = checkpoint_prefix)
-        display.clear_output(wait=True)
-        generate_and_save_images(
-            generator,
-            epochs,
-            seed
-        )
-
-    train(train_dataset, EPOCHS)
+    EPOCHS = 1
+    train(train_dataset, EPOCHS, generator, generator_optimizer, discriminator, discriminator_optimizer)
+    get_anim_file()
